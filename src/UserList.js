923c564526cdc5512eb9b64441848a062afdb092
jeunh <gkwldms1218@gmail.com>
2023-05-16 23:26:26 +0900

    1-22. Context API를 사용한 전역 값 관리

--- src/UserList.js
+++ src/UserList.js
@@ -1,14 +1,9 @@
-import React, { useEffect } from "react";
+import React, { useContext } from "react";
+import { UserDispatch } from "./App";
 
-function User({ user, onRemove, onToggle }) {
-  // useEffect(() => {
-  //   console.log('user 값이 설정됨');
-  //   console.log(user);
-  //   return () => {
-  //     console.log('user가 바뀌기 전..');
-  //     console.log(user);
-  //   };
-  // }, [user]);
+const User = React.memo(function User({ user }) {
+  const dispatch = useContext(UserDispatch);
+
   return (
     <div>
       <b
@@ -16,18 +11,24 @@ function User({ user, onRemove, onToggle }) {
           cursor: 'pointer', 
           color: user.active?'green':'black'
         }}
-        onClick={() => onToggle(user.id)}
+        onClick={() => {
+          dispatch({ type: 'TOGGLE_USER', id: user.id });
+        }}
       >{user.username}</b> <span>({user.email})</span>
-      <button onClick={() => onRemove(user.id)}>삭제</button>
+      <button 
+        onClick={() => {
+          dispatch({ type: 'REMOVE_USER', id: user.id });
+        }} 
+      >삭제</button>
     </div>
   );
-}
+});
 
-function UserList({ users, onRemove, onToggle }) {
+function UserList({ users }) {
   return (
     <div>
       {users.map(user => (
-        <User user={user} key={user.id} onRemove={onRemove} onToggle={onToggle} />
+        <User user={user} key={user.id} />
       ))}
     </div>
   );
